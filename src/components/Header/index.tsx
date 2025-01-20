6bd46e92b5428eed76aa07a70d8a4e257c819f3c
yommexg <hydrokidb@gmail.com>
2025-01-20 13:01:51 +0000

    Home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -26,48 +26,12 @@ const Header = () => {
         backgroundPosition: "center top",
       }}
     >
-      <div className="flex flex-row justify-between items-center ml-2 py-1">
-        <img src={logo} className="w-[112px] h-[83px] p-2" alt="Logo" />
+      <div>
+        <div className="flex flex-row justify-between items-center ml-5 py-1">
+          <img src={logo} className="w-[112px] h-[83px] p-2" alt="Logo" />
 
-        {/* Desktop Nav */}
-        <nav className="hidden md:flex text-white font-montserrat font-[400] text-lg gap-8 mr-10">
-          {[
-            { label: "HOME", href: "#" },
-            { label: "ABOUT", href: "#about" },
-            { label: "PORTFOLIO", href: "#portfolio" },
-            { label: "BOOKING", href: "#bookings" },
-          ].map((link) => (
-            <a
-              key={link.href}
-              href={link.href}
-              className={`${
-                activeLink === link.href
-                  ? "text-[#D13D1F] border-b border-b-[#D13D1F]"
-                  : "text-white"
-              } hover:opacity-60`}
-              onClick={() => handleSetActiveLink(link.href)}
-            >
-              {link.label}
-            </a>
-          ))}
-        </nav>
-
-        {/* Mobile Nav */}
-        <div className="block md:hidden mr-5">
-          {!isMenuOpen && <FaBars size={25} onClick={toggleMenu} />}
-        </div>
-      </div>
-
-      {/* Mobile Menu Overlay */}
-      {isMenuOpen && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-90 z-50 flex flex-col items-center justify-center text-white">
-          <button
-            className="absolute top-5 right-5 text-white"
-            onClick={toggleMenu}
-          >
-            <FaTimes size={25} />
-          </button>
-          <nav className="flex flex-col gap-8 text-xl font-montserrat font-[400]">
+          {/* Desktop Nav */}
+          <nav className="hidden md:flex text-white font-montserrat font-[400] text-lg gap-8 mr-10">
             {[
               { label: "HOME", href: "#" },
               { label: "ABOUT", href: "#about" },
@@ -88,8 +52,60 @@ const Header = () => {
               </a>
             ))}
           </nav>
+
+          {/* Mobile Nav */}
+          <div className="block md:hidden mr-5">
+            {!isMenuOpen && <FaBars size={25} onClick={toggleMenu} />}
+          </div>
         </div>
-      )}
+
+        {/* Mobile Menu Overlay */}
+        {isMenuOpen && (
+          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-90 z-50 flex flex-col items-center justify-center text-white">
+            <button
+              className="absolute top-5 right-5 text-white"
+              onClick={toggleMenu}
+            >
+              <FaTimes size={25} />
+            </button>
+            <nav className="flex flex-col gap-8 text-xl font-montserrat font-[400]">
+              {[
+                { label: "HOME", href: "#" },
+                { label: "ABOUT", href: "#about" },
+                { label: "PORTFOLIO", href: "#portfolio" },
+                { label: "BOOKING", href: "#bookings" },
+              ].map((link) => (
+                <a
+                  key={link.href}
+                  href={link.href}
+                  className={`${
+                    activeLink === link.href
+                      ? "text-[#D13D1F] border-b border-b-[#D13D1F]"
+                      : "text-white"
+                  } hover:opacity-60`}
+                  onClick={() => handleSetActiveLink(link.href)}
+                >
+                  {link.label}
+                </a>
+              ))}
+            </nav>
+          </div>
+        )}
+      </div>
+      <div className="mt-[120px] ml-8 font-montserrat font-[500] text-white">
+        <h1 className="text-[45px] md:text-[54px] font-[600]">
+          DEE UNVEILING <span className="text-[#D13D1F]">STUDIOS</span>
+        </h1>
+        <p className="max-w-[600px] text-lg mt-5">
+          The creative hub for capturing stunning photos and videos, offering
+          professional equipment, versatile setups, and expert services to bring
+          your vision to life.
+        </p>
+
+        <button className="mt-8 px-6 py-2 border-2 rounded-lg border-[#D13D1FCC] ">
+          CONTACT
+        </button>
+      </div>
     </div>
   );
 };
