5d02749c7fee81ead3e3c8ddfa5543f3deec855c
yommexg <hydrokidb@gmail.com>
2025-01-20 03:28:17 +0000

    Update Header bg

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,16 @@
-// const video = new URL("../../assets/video-bg.jpg", import.meta.url).href;
+const headerbg = new URL("../../assets/header-bg.jpg", import.meta.url).href;
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div
+      className="min-h-screen bg-center sm:bg-[auto_110%] bg-cover"
+      style={{
+        backgroundImage: `url(${headerbg})`,
+      }}
+    >
+      {/* Your header content here */}
+    </div>
+  );
 };
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
+  safelist: ["bg-[url()]"],
   theme: {
     extend: {},
   },
