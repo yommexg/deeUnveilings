865b1778790fd23a9290fe9533f81896255f7920
yommexg <hydrokidb@gmail.com>
2025-01-21 05:46:51 +0000

    Portfolio

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -7,7 +7,7 @@ const About = () => {
   return (
     <div
       id="about"
-      className="min-h-screen flex justify-ceter items-end flex-col md:px-8 px-4 pr-20  md:py-[300px]"
+      className="min-h-screen flex justify-ceter items-end flex-col md:px-8 px-4 pr-20  md:pt-[350px] md:pb-[150px]"
     >
       <div className="w-full flex flex-row items-center justify-between gap-4 md:gap-10 relative">
         {/* Animated Image 1 */}
--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,5 +1,21 @@
+import { motion } from "framer-motion";
+
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <div id="portfolio" className="md:px-8 px-4 pr-20 min-h-screen pt-[100px]">
+      <div className="flex items-center gap-4 md:gap-10">
+        <motion.h2
+          className="font-[500] text-[20px] md:text-[40px]"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.6, delay: 0.1 }}
+        >
+          Portfolio
+        </motion.h2>
+        <div className="flex-1 h-[3px] bg-[#D13D1F]" />
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
